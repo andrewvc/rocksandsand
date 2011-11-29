501c8f4143b71609889e98fef4a443a0b4d66c9e
Andrew Cholakian <andrew@andrewvc.com>
2011-11-29 08:17:00 -0800

    Display recent gardens in the proper order on the homepage

--- src/rocksandsand/views/index.clj
+++ src/rocksandsand/views/index.clj
@@ -14,5 +14,5 @@
     [:h2 "&#9673; Recent Gardens"]
     [:ul {:id "recent"}
       (map (fn [uuid] [:li (link-to (str "/gardens/" uuid)  uuid)])
-           (garden/recent))]
+           (reverse (garden/recent)))]
     (link-to "/about" "◪ About")))
