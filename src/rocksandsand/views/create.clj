84b41ea58c52cfb0716f7d248be163e1638f3033
Andrew Cholakian <andrew@andrewvc.com>
2011-12-02 08:47:21 -0800

    Added instructions to create page

--- src/rocksandsand/views/create.clj
+++ src/rocksandsand/views/create.clj
@@ -7,6 +7,10 @@
 
 (defpage "/create" []
   (common/layout
+    [:div {:id "instructions"}
+      "Click/touch and drag on the sand area below to rake."
+      [:br]
+      "You may also place rocks from the selection below."]
     [:canvas {:id "garden" :width "760" :height "520"}]
     [:div {:id "rock-caddy"}]
     [:a {:href "#" :id "complete"} "&#9632; Complete and Save"]))
